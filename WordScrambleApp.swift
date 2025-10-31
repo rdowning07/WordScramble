2f70847092447d489bc6afea1d60e6e9740572aa
Rob Downing <redowning@gmail.com>
2025-10-30 22:03:46 -0700

    Initial commit: WordScramble SwiftUI project setup

---
+++ WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Rob Downing on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
